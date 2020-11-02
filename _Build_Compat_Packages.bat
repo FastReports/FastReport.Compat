3ed5ecf8cd2ac1f1b166a2b6e1437d098f5d8927
Kirill Kornienko <k.kornienko@fast-report.com>
2020-11-02 13:38:26 +0300

    added build batch file

---
+++ _Build_Compat_Packages.bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+rem HELP FOR THIS FILE
+rem CLONE the fr if need
+rem clear the fr if need
+rem next run scripts from the fr tools
+rem that's all
+
+ECHO TRY TO BUILD FR.Compat
+
+pushd .\build
+   Powershell -ExecutionPolicy ByPass -File ".\build.ps1" --target=Compat --solution-filename=FastReport.Compat.sln --config=Release  --vers=2020.4.0
+popd
