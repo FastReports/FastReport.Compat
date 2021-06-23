c5553d18049567fdb6928ec7b5af1fb50c2db28f
Kirill Kornienko <k.kornienko@fast-report.com>
2021-06-23 11:37:23 +0300

    Updated report script compilation (more changes: SFA support, fixed load user libraries in runtime, using AssemblyLoadContext etc)

--- src/shared/DotNetClasses/CodeDomProvider.cs
+++ src/shared/DotNetClasses/CodeDomProvider.cs
@@ -1,18 +1,27 @@
 ﻿#if NETSTANDARD2_0 || NETSTANDARD2_1 || NETCOREAPP
 using Microsoft.CodeAnalysis;
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Reflection;
+using System.Linq;
+using System.Reflection.Metadata;
+#if NETCOREAPP
+using System.Runtime.Loader;
+#endif
 
 namespace FastReport.Code.CodeDom.Compiler
 {
     public abstract class CodeDomProvider : IDisposable
     {
-        static readonly Dictionary<string, string> cache = new Dictionary<string, string>();
+        static readonly Dictionary<string, MetadataReference> cache = new Dictionary<string, MetadataReference>();
 
         public abstract void Dispose();
         public abstract CompilerResults CompileAssemblyFromSource(CompilerParameters cp, string v);
+
+        public event EventHandler<CompilationEventArgs> BeforeEmitCompilation;
+
 
         protected void AddExtraAssemblies(StringCollection referencedAssemblies, List<MetadataReference> references)
         {
@@ -47,42 +56,164 @@ namespace FastReport.Code.CodeDom.Compiler
             }
         }
 
+        internal void OnBeforeEmitCompilation(Compilation compilation)
+        {
+            if (BeforeEmitCompilation != null)
+            {
+                var eventArgs = new CompilationEventArgs
+                    { Compilation = compilation };
+                BeforeEmitCompilation(this, eventArgs);
+            }
+        }
 
         public MetadataReference GetReference(string refDll)
         {
             string reference = refDll;
+            MetadataReference result;
             try
             {
                 if (cache.ContainsKey(refDll))
-                    return MetadataReference.CreateFromFile(cache[refDll]);
-                MetadataReference result;
-                foreach (AssemblyName name in Assembly.GetExecutingAssembly().GetReferencedAssemblies())
+                    return cache[refDll];
+
+                reference = refDll.EndsWith(".dll") || refDll.EndsWith(".exe") ?
+                    refDll.Substring(0, refDll.Length - 4) : refDll;
+                if (!refDll.Contains(Path.DirectorySeparatorChar))
                 {
-                    if (name.Name == reference
-                        || reference.ToLower().EndsWith(".dll")
-                        && name.Name == reference.Substring(0, reference.Length - 4))
+#if NETCOREAPP
+                    // try find in AssemblyLoadContext
+                    foreach (AssemblyLoadContext assemblyLoadContext in AssemblyLoadContext.All)
                     {
-                        result = MetadataReference.CreateFromFile(
-                            Assembly.Load(name).Location);
-                        cache[refDll] = reference;
-                        return result;
+                        foreach (Assembly loadedAssembly in assemblyLoadContext.Assemblies)
+                        {
+                            if (loadedAssembly.GetName().Name == reference)
+                            {
+                                string location = loadedAssembly.Location;
+                                if (string.IsNullOrEmpty(location))
+                                {
+                                    result = GetMetadataReferenceInSingleFileApp(loadedAssembly);
+                                }
+                                else
+                                {
+                                    result = MetadataReference.CreateFromFile(location);
+                                }
+
+                                cache[refDll] = result;
+                                return result;
+                            }
+                        }
+                    }
+#else
+                    foreach (Assembly currAssembly in AppDomain.CurrentDomain.GetAssemblies())
+                    {
+                        if (string.Compare(currAssembly.GetName().Name, reference, true) == 0)
+                        {
+                            // Found it, return the location as the full reference.
+                            result = MetadataReference.CreateFromFile(currAssembly.Location);
+                            cache[refDll] = result;
+                            return result;
+                        }
+                    }
+#endif
+                    // try find in ReferencedAssemblies
+                    foreach (AssemblyName name in Assembly.GetExecutingAssembly().GetReferencedAssemblies())
+                    {
+                        if (name.Name == reference)
+                        {
+#if NETCOREAPP
+                            // try load Assembly in runtime (for user script with custom assembly)
+                            var assembly = AssemblyLoadContext.Default.LoadFromAssemblyName(name);
+#else
+                            var assembly = Assembly.Load(name);
+#endif
+
+                            string location = assembly.Location;
+
+#if NETCOREAPP
+                            if (string.IsNullOrEmpty(location))
+                            {
+                                result = GetMetadataReferenceInSingleFileApp(assembly);
+                            }
+                            else
+#endif
+                            {
+                                result = MetadataReference.CreateFromFile(location);
+                            }
+
+                            cache[refDll] = result;
+                            return result;
+                        }
                     }
                 }
+                
 
-                result = MetadataReference.CreateFromFile(reference);
-                cache[refDll] = reference;
+                result = MetadataReference.CreateFromFile(refDll);
+#if NETCOREAPP
+                try
+                {
+                    // try load Assembly in runtime (for user script with custom assembly)
+                    AssemblyLoadContext.Default.LoadFromAssemblyPath(refDll);
+                }
+                catch(ArgumentException) {
+                    var fullpath = Path.Combine(Environment.CurrentDirectory, refDll);
+                    try
+                    {
+                        AssemblyLoadContext.Default.LoadFromAssemblyPath(fullpath);
+                    }
+                    catch { }
+                }
+                catch { }
+#endif
+                cache[refDll] = result;
+                
                 return result;
             }
             catch
             {
-                string ass = reference;
-                if (reference.ToLower().EndsWith(".dll"))
-                    ass = reference.Substring(0, reference.Length - 4);
-                string location = Assembly.Load(new AssemblyName(ass)).Location;
-                cache[refDll] = location;
-                return MetadataReference.CreateFromFile(location);
-            }
+                var assemblyName = new AssemblyName(reference);
+#if NETCOREAPP
+                // try load Assembly in runtime (for user script with custom assembly)
+                var assembly = AssemblyLoadContext.Default.LoadFromAssemblyName(assemblyName);
+#else
+                var assembly = Assembly.Load(assemblyName);
+#endif
 
+                string location = assembly.Location;
+
+                result = MetadataReference.CreateFromFile(location);
+                cache[refDll] = result;
+                return result;
+            }
+        }
+
+#if NETCOREAPP
+        private static unsafe MetadataReference GetMetadataReferenceInSingleFileApp(Assembly assembly)
+        {
+            assembly.TryGetRawMetadata(out byte* blob, out int length);
+            var moduleMetadata = ModuleMetadata.CreateFromMetadata((IntPtr)blob, length);
+            var assemblyMetadata = AssemblyMetadata.Create(moduleMetadata);
+            return assemblyMetadata.GetReference();
+        }
+#endif
+
+        public static string TryFixReferenceInSingeFileApp(Assembly assembly)
+        {
+#if NETCOREAPP
+            try
+            {
+                string assemblyName = assembly.GetName().Name;
+                if(!cache.ContainsKey(assemblyName))
+                {
+                    MetadataReference metadataReference = GetMetadataReferenceInSingleFileApp(assembly);
+                    cache[assemblyName] = metadataReference;
+                }
+                return assemblyName;
+            }
+            catch
+            {
+
+            }
+#endif
+            return null;
         }
 
         //public IEnumerable<MetadataReference> GetReferences(string[] refsDll)
